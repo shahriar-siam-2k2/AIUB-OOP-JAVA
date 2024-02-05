22850ee4523549606527a9d9152984597bb1513f
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-02-06 01:31:11 +0600

    06-02-24

---
+++ Student Details/stuDetails.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class stuDetails {
+	public static void main(String[] args) {
+		System.out.println("\nStudent Details-\n");
+		System.out.println("Academic-");
+		System.out.println("Name: Shahriar Ali Siam");
+		System.out.println("ID: 23-53004-3");
+		System.out.println("CGPA: 3.75");
+		System.out.println("University: American International University Bangladesh-AIUB");
+		System.out.println("\nAddress-");
+		System.out.println("Home Town: Pabna");
+		System.out.println("\nOthers-");
+		System.out.println("Hobby: Programming");
+	}
+}
---
+++ Test/test.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+
+public class test{
+    public static void main(String[] args){
+        System.out.println("\nHELLO SIAM");
+    }
+}
