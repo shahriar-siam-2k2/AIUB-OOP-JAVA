a947debeb885b2aa29a3652d4b625af1934bdff0
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-02-22 23:33:51 +0600

    20-FEB-24 LAB

---
+++ LAB Tasks/LAB-04_Scanner [20-FEB-24]/Person with Scanner/Start.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+
+public class Start{
+	public static void main(String[] args){
+		Scanner sc1 = new Scanner(System.in);
+		System.out.print("\nEnter Name: ");
+		String nm = sc1.nextLine();
+		
+		Scanner sc2 = new Scanner(System.in);
+		System.out.print("Enter ID: ");
+		String ID = sc2.nextLine();
+		
+		System.out.print("Enter Age: ");
+		int ag = sc1.nextInt();
+		
+		Person p1 = new Person(nm, ID, ag);
+		System.out.printlnn("\nPerson-1 Details: ");
+		p1.Details();
+		
+		Person p2 = new Person();
+		System.out.println("\nPerson-2 Details: ");
+		p2.Details();
+	}
+}
