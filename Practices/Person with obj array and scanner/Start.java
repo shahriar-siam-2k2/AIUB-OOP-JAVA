79ede548fe84af18d8efa5eceab105edece4b994
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-03-01 14:08:32 +0600

    Update Start.java
    Fixed bug of person class's start.java

--- Practices/Person with obj array and scanner/Start.java
+++ Practices/Person with obj array and scanner/Start.java
@@ -12,24 +12,21 @@ public class Start{
 		size = sc.nextInt();
 		
 		Person per[] = new Person [size];
-		Scanner str[] = new Scanner(System.in) [size];
 		
 		for(int i=0 ; i<size ; i++){
 			System.out.println("\nPerson-" + (i+1) + " Information setting-");
 			
-			if(str[i] == null){
-				System.out.print("Enter Name: ");
-				nm = str[i].nextLine();
-				System.out.print("Enter ID: ");
-				id = str[i].nextLine();
-			}
+			System.out.print("Enter Name: ");
+			nm = sc.next();
+			System.out.print("Enter ID: ");
+			id = sc.next();
 			System.out.print("Enter Age: ");
 			ag = sc.nextInt();
-			
 			if(per[i] == null){
 				per[i] = new Person(nm, id, ag);
 			}
 		}
+		
 		System.out.println("\nAll person details-");
 		for(int i=0 ; i<size ; i++){
 			System.out.println("\nPerson-" + (i+1) + ":");
